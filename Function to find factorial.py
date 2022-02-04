bd02202d503d055e5c6f3b561c7edabb20971ab5
Code4uu <99035498+Code4uu@users.noreply.github.com>
2022-02-04 22:01:13 +0530

    Day 4 submitted

---
+++ Function to find factorial.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(fact(n))
